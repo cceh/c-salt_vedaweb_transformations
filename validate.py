f36d110dd9da77a4a01db4cb822a7b9d66376c09
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-12-10 20:49:49 +0100

    cli validation added

--- validate.py
+++ validate.py
@@ -1,5 +1,6 @@
 from lxml import etree
 import pathlib
+import argparse
 
 
 def validate(file, schema):
@@ -9,9 +10,19 @@ def validate(file, schema):
     return relaxng.validate(doc)
 
 
-def validate_all(path_to_tei_files):
+def validate_all(args):
+    path_to_tei_files = args.csalt_vedaweb_tei
     files = pathlib.Path(path_to_tei_files).glob('*rv_book*')
     for f in files:
         valid = validate(str(f), path_to_tei_files + '/vedaweb.rng')
-        if not valid:
+        if valid:
             print(f, valid)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('csalt_vedaweb_tei',
+                        help='path to c-salt_vedaweb_tei')
+    args = parser.parse_args()
+    validate_all(args)
